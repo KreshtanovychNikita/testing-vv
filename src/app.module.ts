4edd31aa73449d273bdeb24e58a4857dfad38144
Nikita <kreshtanovych.mykyta@chnu.edu.ua>
2024-02-12 19:57:08 +0200

    psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,19 @@ import { TelegramSessionEntity } from './entities/telegram-session.entity';
 @Module({
   imports: [
     ConfigModule.forRoot({}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [TelegramSessionEntity],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [TelegramSessionEntity],
+        synchronize: true,
+      }),
     }),
     TypeOrmModule.forFeature([TelegramSessionEntity])
   ],
